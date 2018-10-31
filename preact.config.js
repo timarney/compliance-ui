cf031195f472cb0e638df41e43116e718985f72d
Tim Arney <tim@line37.com>
2018-10-31 13:51:41 -0400

    add public path

--- preact.config.js
+++ preact.config.js
@@ -2,6 +2,9 @@ import envVars from "preact-cli-plugin-env-vars";
 
 export default (config, env, helpers) => {
   envVars(config, env, helpers);
+  config.output.publicPath = process.env.PREACT_APP_PUBLIC_PATH;
+  console.log("app path:", config.output.publicPath);
+
   if (env.isProd) {
     // Make async work
     let babel = config.module.loaders.filter(
